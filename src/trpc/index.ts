e4462a4a27f8c83b672bcf6ef111b3946d1bccf1
JiangShuuu <globelex65@gmail.com>
2023-11-12 10:20:04 +0800

    aad stripe

--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -5,8 +5,8 @@ import { db } from '@/db'
 import { z } from 'zod'
 import { absoluteUrl } from '@/lib/utils'
 import { INFINITE_QUERY_LIMIT } from '@/config/infinite-query'
-// import { getUserSubscriptionPlan, stripe } from '@/lib/stripe'
-// import { PLANS } from '@/config/stripe'
+import { getUserSubscriptionPlan, stripe } from '@/lib/stripe'
+import { PLANS } from '@/config/stripe'
 
 export const appRouter = router({
   authCallback: publicProcedure.query(async () => {
@@ -59,35 +59,35 @@ export const appRouter = router({
 
     if (!dbUser) throw new TRPCError({ code: 'UNAUTHORIZED' })
 
-    // const subscriptionPlan = await getUserSubscriptionPlan()
+    const subscriptionPlan = await getUserSubscriptionPlan()
 
-    // if (subscriptionPlan.isSubscribed && dbUser.stripeCustomerId) {
-    //   const stripeSession = await stripe.billingPortal.sessions.create({
-    //     customer: dbUser.stripeCustomerId,
-    //     return_url: billingUrl
-    //   })
+    if (subscriptionPlan.isSubscribed && dbUser.stripeCustomerId) {
+      const stripeSession = await stripe.billingPortal.sessions.create({
+        customer: dbUser.stripeCustomerId,
+        return_url: billingUrl
+      })
 
-    //   return { url: stripeSession.url }
-    // }
+      return { url: stripeSession.url }
+    }
 
-    // const stripeSession = await stripe.checkout.sessions.create({
-    //   success_url: billingUrl,
-    //   cancel_url: billingUrl,
-    //   payment_method_types: ['card', 'paypal'],
-    //   mode: 'subscription',
-    //   billing_address_collection: 'auto',
-    //   line_items: [
-    //     {
-    //       price: PLANS.find((plan) => plan.name === 'Pro')?.price.priceIds.test,
-    //       quantity: 1
-    //     }
-    //   ],
-    //   metadata: {
-    //     userId: userId
-    //   }
-    // })
+    const stripeSession = await stripe.checkout.sessions.create({
+      success_url: billingUrl,
+      cancel_url: billingUrl,
+      payment_method_types: ['card'],
+      mode: 'subscription',
+      billing_address_collection: 'auto',
+      line_items: [
+        {
+          price: PLANS.find((plan) => plan.name === 'Pro')?.price.priceIds.test,
+          quantity: 1
+        }
+      ],
+      metadata: {
+        userId: userId
+      }
+    })
 
-    // return { url: stripeSession.url }
+    return { url: stripeSession.url }
   }),
 
   getFileMessages: privateProcedure
