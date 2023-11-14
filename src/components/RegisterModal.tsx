ee6b66fc65061ba277a747588dc3667d36097a48
JiangShuuu <globelex65@gmail.com>
2023-11-14 17:27:23 +0800

    upd register

--- src/components/RegisterModal.tsx
+++ src/components/RegisterModal.tsx
@@ -5,77 +5,143 @@ import {
   DialogDescription,
   DialogFooter,
   DialogHeader,
-  DialogTitle,
-  DialogTrigger
+  DialogTitle
 } from '@/components/ui/dialog'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 import { FieldValues, SubmitHandler, useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import * as z from 'zod'
+import { trpc } from '@/app/_trpc/client'
+import { useToast } from './ui/use-toast'
 
 interface RegisterModalProps {
   open: boolean
   setOpen: React.Dispatch<React.SetStateAction<boolean>>
 }
 
+const formSchema = z.object({
+  email: z.string().email(),
+  name: z.string().min(2, {
+    message: 'Name must be at least 2 characters.'
+  }),
+  password: z.string().min(8, {
+    message: 'Password must be at least 8 characters.'
+  })
+})
+
 export default function RegisterModal({ open, setOpen }: RegisterModalProps) {
   const {
     register,
+    reset,
     handleSubmit,
     formState: { errors }
   } = useForm<FieldValues>({
+    resolver: zodResolver(formSchema),
     defaultValues: {
       email: '',
-      username: '',
+      name: '',
       password: ''
     }
   })
 
-  const onSubmit: SubmitHandler<FieldValues> = () => {
-    console.log('success')
+  const { toast } = useToast()
+
+  const mutation = trpc.registerAccount.useMutation({
+    onSuccess: (result) => {
+      if (result.success) {
+        toast({
+          title: 'Register Success, Pleace SignIn!'
+        })
+      }
+      change()
+    },
+    onError: (result) => {
+      console.error(result)
+      change()
+    }
+  })
+
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    mutation.mutate({
+      email: data.email,
+      name: data.name,
+      password: data.password
+    })
+  }
+
+  const change = () => {
+    reset()
+    setOpen(!open)
   }
 
   return (
-    <Dialog open={open} onOpenChange={setOpen}>
-      <DialogContent className="sm:max-w-[425px]">
+    <Dialog open={open} onOpenChange={change}>
+      <DialogContent>
         <DialogHeader>
           <DialogTitle>Register Account</DialogTitle>
           <DialogDescription>
             {/* eslint-disable-next-line react/no-unescaped-entities */}
-            Make register to your account here. Click save when you're done.
+            Register to your account here. Click save when you're done.
           </DialogDescription>
         </DialogHeader>
-        <div className="grid gap-4 py-4">
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="email" className="text-right">
-              Email
-            </Label>
-            <Input
-              id="email"
-              {...register('email', { required: true })}
-              defaultValue=""
-              className="col-span-3"
-              placeholder="example@gmail.com"
-            />
-            {errors.email && <span>This field is required</span>}
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="grid gap-7 py-4">
+            <div className="relative grid grid-cols-4 items-center gap-4">
+              <Label htmlFor="email" className="text-right">
+                Email
+              </Label>
+              <Input
+                id="email"
+                {...register('email', { required: true })}
+                defaultValue=""
+                className="col-span-3"
+                placeholder="example@gmail.com"
+              />
+              {errors.email && (
+                <p className="absolute -bottom-5 right-0 text-xs text-red-500">
+                  {JSON.stringify(errors.email.message)}
+                </p>
+              )}
+            </div>
+            <div className="relative grid grid-cols-4 items-center gap-4">
+              <Label htmlFor="name" className="text-right">
+                Name
+              </Label>
+              <Input
+                id="name"
+                {...register('name', { required: true })}
+                defaultValue=""
+                className="col-span-3"
+                placeholder="John"
+              />
+              {errors.name && (
+                <p className="absolute -bottom-5 right-0 text-xs text-red-500">
+                  {JSON.stringify(errors.name.message)}
+                </p>
+              )}
+            </div>
+            <div className="relative grid grid-cols-4 items-center gap-4">
+              <Label htmlFor="password" className="text-right">
+                Password
+              </Label>
+              <Input
+                id="password"
+                {...register('password', { required: true })}
+                className="col-span-3"
+                type="password"
+              />
+              {errors.password && (
+                <p className="absolute -bottom-5 right-0 text-xs text-red-500">
+                  {JSON.stringify(errors.password.message)}
+                </p>
+              )}
+            </div>
+            <div className="mt-4 text-right">
+              <Button type="submit">Register</Button>
+            </div>
           </div>
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="username" className="text-right">
-              Username
-            </Label>
-            <Input id="username" defaultValue="" className="col-span-3" placeholder="John" />
-          </div>
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="password" className="text-right">
-              Password
-            </Label>
-            <Input id="password" className="col-span-3" type="password" />
-          </div>
-        </div>
-        <DialogFooter>
-          <Button type="submit" onClick={handleSubmit(onSubmit)}>
-            Save changes
-          </Button>
-        </DialogFooter>
+        </form>
       </DialogContent>
     </Dialog>
   )
