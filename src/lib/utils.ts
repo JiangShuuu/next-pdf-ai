e8a6d4f56e2a681355c67a0816bbfec03e0a887d
JiangShuuu <globelex65@gmail.com>
2023-11-16 10:53:01 +0800

    upd env

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -8,12 +8,13 @@ export function cn(...inputs: ClassValue[]) {
 
 export function absoluteUrl(path: string) {
   if (typeof window !== 'undefined') return path
-  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}${path}`
+  // if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}${path}`
+  if (process.env.BASE_URL) return `https://${process.env.BASE_URL}${path}`
   return `http://localhost:${process.env.PORT ?? 3000}${path}`
 }
 
 export function constructMetadata({
-  title = 'PDF-Ai - the SaaS for students',
+  title = 'PDF-Ai - the Ai Chat SaaS ',
   description = 'Quill is an open-source software to make chatting to your PDF files easy.',
   image = '/thumbnail.png',
   icons = '/favicon.ico',
@@ -45,7 +46,7 @@ export function constructMetadata({
       creator: '@joshtriedcoding'
     },
     icons,
-    // metadataBase: new URL(`${process.env.KINDE_SITE_URL}`),
+    metadataBase: new URL(`${process.env.BASE_URL}`),
     themeColor: '#FFF',
     ...(noIndex && {
       robots: {
