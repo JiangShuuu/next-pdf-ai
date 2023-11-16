7bc2168f96390d8893ec84e61fea9ca10aaa2a87
JiangShuuu <globelex65@gmail.com>
2023-11-16 22:20:03 +0800

    fix uploadthing

--- src/app/api/uploadthing/route.ts
+++ src/app/api/uploadthing/route.ts
@@ -4,5 +4,10 @@ import { ourFileRouter } from './core'
 
 // Export routes for Next App Router
 export const { GET, POST } = createNextRouteHandler({
-  router: ourFileRouter
+  router: ourFileRouter,
+  config: {
+    uploadthingId: process.env.UPLOADTHING_APP_ID,
+    uploadthingSecret: process.env.UPLOADTHING_SECRET,
+    callbackUrl: `${process.env.NEXT_PUBLIC_BASE_URL}/api/uploadthing`
+  }
 })
